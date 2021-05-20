c727f085598cf4282b3b7879daee577d5a74ea8b
ginochasles <ginochasles@gmail.com>
2021-05-20 21:05:33 +0200

    link dans navbar

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <NavBar />
      <Switch>
         <Route path="/cv">
-        <iframe src={process.env.PUBLIC_URL + "CV_Chasles_Gino.pdf#view=Fit"} title="cv">
+        <iframe src="CV_Chasles_Gino.pdf#view=Fit" title="cv">
     </iframe>
         
           <Profil />
