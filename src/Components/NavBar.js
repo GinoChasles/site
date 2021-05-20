13787b404d309d0af907f681a0c7a38fb9ccc00a
ginochasles <ginochasles@gmail.com>
2021-05-20 18:09:14 +0200

    link dans navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -23,7 +23,7 @@ export default class NavBar extends Component {
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <Link className={item.cName} href={item.url}>
+                                <Link className={item.cName} to={item.url}>
                                 {item.title}
                                 </Link>
                             </li>
