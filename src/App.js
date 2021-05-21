f246c8e9401b9c495eb34807f110dbf1c65869ed
ginochasles <ginochasles@gmail.com>
2021-05-21 17:11:25 +0200

    importation et vu CV dans l'onglet cv du site

--- src/App.js
+++ src/App.js
@@ -20,25 +20,10 @@ function App() {
       <NavBar />
      <Switch>
         <Route path="/cv">
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf"} height="500px" width="100%" title="cv"></embed>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=FitBH"} height="500px" width="100%" title="cv"></embed>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=Fit"} height="500px" width="100%" title="cv"></embed>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=FitH"} height="500px" width="100%" title="cv"></embed>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=FitV"} height="500px" width="100%" title="cv"></embed>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=FitB"} height="500px" width="100%" title="cv"></embed>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=FitBV"} height="500px" width="100%" title="cv"></embed>
-       
-        <object type="application/pdf" data={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf"} height="500px" width="100%" title="cv"></object>
-        <object type="application/pdf" data="site/images/CV_Chasles_Gino.pdf" height="500px" width="100%" title="cv"></object>
-          
-          <Formation />
+         <embed type="application/pdf" src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino.pdf#view=FitH"}  title="cv"></embed>
         </Route>
-        <Route path="/portfolio"><Profil/></Route>
-        <Route path="/passions" ><div>portfolio</div>
-        <embed type="application/pdf" src={process.env.PUBLIC_URL + "/CV_Chasles_Gino.pdf#zoom=50"} height="500px" width="100%" title="cv"></embed>
-        <img src={process.env.PUBLIC_URL + "/CV_Chasles_Gino_OC.png"} height="50%" width="80%" title="cv" alt="cv"></img>
-        <img src={process.env.PUBLIC_URL + "/images/CV_Chasles_Gino_OC.png"} height="50%" width="80%" title="cv" alt="cv"></img>
-    </Route>
+        <Route path="/portfolio"></Route>
+        <Route path="/passions" ></Route>
         <Route path="/contact"><div>contact</div></Route>
 
      </Switch>
